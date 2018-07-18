b05feacd68f73bea67b4ce8d857d6b057147b1c4
Danny Chang <dannychachang@gmail.com>
2018-07-18 16:55:12 -0400

    learn to setState and props, things that require refresh

--- starting_off/src/App.js
+++ starting_off/src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "what", age: 100},
+      { name: "who", age: 1023},
+      { name: "whey", age: 10}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "What", age: 10230},
+        { name: "Who", age: 1023},
+        { name: "Whey", age: 42410}
+      ]
+    })
+    // DONT DO this.state.persons[0].name = "poopooo"
+    // console.log('Was clicked!')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi its me and I am a React App</h1>
         <p>Works with two elements</p>
-        <Person name="Danny" age='23'/>
-        <Person name="Dannee" age='29'>My Hobbies: chillin</Person>
-        <Person name="POoema" age='203'/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi it\'s me and I am a React App!!"))
