f6f77ba7cce7f9b2ffdb44406032f64c908c6b14
Danny Chang <dannychachang@gmail.com>
2018-07-19 14:37:28 -0400

    if statements outside of return

--- starting_off/src/App.js
+++ starting_off/src/App.js
@@ -51,6 +51,28 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Deennee")}
+            changed={this.nameChangeHandler} >My Hobbies: Chillin </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi its me and I am a React App</h1>
@@ -58,24 +80,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Show Names</button>
-        {
-          this.state.showPersons ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Deennee")}
-              changed={this.nameChangeHandler} >My Hobbies: Chillin </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div> : null
-        }
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi it\'s me and I am a React App!!"))
