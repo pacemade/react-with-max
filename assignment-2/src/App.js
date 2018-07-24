6966d47da7d45e6686808ccfd3c4e56d1c02f830
Danny Chang <dannychachang@gmail.com>
2018-07-24 12:21:00 -0400

    assignment-2 round one finished

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import Validation from './ValidationComponent/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
-    length: 0
+    userInput: ''
   }
 
-  countLengthHandler = () => {
-    console.log("working");
-  }
+  inputChangedHandler = (event) => {
+    this.setState({
+      userInput: event.target.value
+    })
+  };
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  };
 
   render() {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />
+    });
+
     return (
       <div>
-        <h1>Length of text: {this.state.length}</h1>
+        <h1>Length of text: {this.state.userInput}</h1>
         <input
           type="text"
-          onChange={() => this.countLengthHandler()} />
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput} />
+          <p>{this.state.userInput}</p>
+          <Validation inputLength={this.state.userInput.length}/>
+          {charList}
       </div>
     )
   }
