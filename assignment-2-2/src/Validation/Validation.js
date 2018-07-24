32d4e43d09bdd1d96efd192636747770f150d7a6
Danny Chang <dannychachang@gmail.com>
2018-07-24 12:57:05 -0400

    validation works with if statement

--- assignment-2-2/src/Validation/Validation.js
+++ assignment-2-2/src/Validation/Validation.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const validation = (props) => {
+  let validationMessage = "Text is too short!"
+
+  if (props.charLength >= 5) {
+    validationMessage = "Text is gud buddy!"
+  };
+
   return(
     <div>
       <p>Number of letters: {props.charLength}</p>
+      <p>{validationMessage}</p>
+
     </div>
   )
 };
