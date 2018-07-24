9bd9c4c644bbd84735aae1b4f296d3ea75631a3b
Danny Chang <dannychachang@gmail.com>
2018-07-24 13:11:30 -0400

    char list working

--- assignment-2-2/src/App.js
+++ assignment-2-2/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Validation from './Validation/Validation'
+import Validation from './Validation/Validation';
+import Char from './Char/Char'
 
 class App extends Component {
   state = {
@@ -12,12 +13,18 @@ class App extends Component {
   };
 
   render() {
+    const charsList = this.state.userInput.split('').map(char => {
+      return <Char
+        ch={char}/>
+    })
+
     return (
       <div>
         <input
           type="text"
           onChange={this.inputChangedHandler}/>
         <Validation charLength={this.state.userInput.length}/>
+        {charsList}
       </div>
     );
   }
