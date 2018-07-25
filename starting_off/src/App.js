b689fe2f156eef1d0b53fea66ae72db15a79c5e3
Danny Chang <dannychachang@gmail.com>
2018-07-25 14:14:11 -0400

    using errorboundary to find problematic components

--- starting_off/src/App.js
+++ starting_off/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 
 class App extends Component {
@@ -23,7 +24,7 @@ class App extends Component {
       ...this.state.persons[personIndex]
     };
 
-    person.name = event.target.value;
+    person.name = event.target .value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
@@ -70,12 +71,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}><Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id}
               changed={(event) => this.nameChangeHandler(event, person.id)} />
+            </ ErrorBoundary>
           })}
         </div>
       );
