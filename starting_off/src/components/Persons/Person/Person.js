4e858a4fba5eaa3ac0461e50a5a331ca792e671b
Danny Chang <dannychachang@gmail.com>
2018-08-01 17:15:20 -0400

    use aux component to replace div

--- starting_off/src/components/Persons/Person/Person.js
+++ starting_off/src/components/Persons/Person/Person.js
@@ -18,18 +18,18 @@ class Person extends Component {
 
   render () {
     console.log('[Person.js] Inside render()');
-    // return (
-    //   <div className={classes.Person}>
-    //     <p onClick={this.props.click}>Im {this.props.name} and I am {this.props.age} years old!</p>
-    //     <p>{this.props.children}</p>
-    //     <input type="text" onChange={this.props.changed} value={this.props.name} />
-    //   </div>
-    // )
-    return [
-      <p key='1' onClick={this.props.click}>Im {this.props.name} and I am {this.props.age} years old!</p>,
-      <p key='2'>{this.props.children}</p>,
-      <input key='3' type="text" onChange={this.props.changed} value={this.props.name} />
-    ]
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>Im {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    )
+    // return [
+    //   <p key='1' onClick={this.props.click}>Im {this.props.name} and I am {this.props.age} years old!</p>,
+    //   <p key='2'>{this.props.children}</p>,
+    //   <input key='3' type="text" onChange={this.props.changed} value={this.props.name} />
+    // ]
   };
 }
 
