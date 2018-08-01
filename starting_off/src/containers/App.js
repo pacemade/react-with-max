b3b51a3e256bfb62a76196d4ba05b2fce9c2f2f6
Danny Chang <dannychachang@gmail.com>
2018-08-01 17:21:34 -0400

    use withclass to reapply css class names

--- starting_off/src/containers/App.js
+++ starting_off/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
+import WithClass from '../hoc/WithClass'
 
 
 
@@ -103,15 +104,15 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
-      <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
-      <Cockpit
-        showPersons={this.state.showPersons}
-        persons={this.state.persons}
-        clicked={this.togglePersonsHandler}
-        appTitle={this.props.title} />
-        {persons}
-      </div>
+      <WithClass classes={classes.App}>
+        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+          appTitle={this.props.title} />
+          {persons}
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi it\'s me and I am a React App!!"))
   }
